2573627c7eeac7d0f8bf44c83b29d00075c4b14b
Himanshu Tomar <eternal_mangekyo@Himanshus-MacBook-Pro.local>
2019-06-08 01:11:54 +0530

    Fixed all the yarn workspaces

---
+++ packages/app/ios/build/app/Build/Intermediates.noindex/RNGestureHandler.build/Debug-iphonesimulator/RNGestureHandler.build/Objects-normal/x86_64/RNForceTouchHandler.d
@@ -0,0 +1,27 @@
+dependencies: \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/Handlers/RNForceTouchHandler.m \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/Handlers/RNForceTouchHandler.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandler.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandlerState.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandlerDirection.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandlerEvents.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTEventDispatcher.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTConvert.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTAnimationType.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTTextDecorationLineType.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/yoga/YGValue.h
---
+++ packages/app/ios/build/app/Build/Intermediates.noindex/RNGestureHandler.build/Debug-iphonesimulator/RNGestureHandler.build/Objects-normal/x86_64/RNGestureHandlerEvents.d
@@ -0,0 +1,12 @@
+dependencies: \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandlerEvents.m \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandlerEvents.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTEventDispatcher.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/ios/build/app/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
+  /Users/eternal_mangekyo/Desktop/Juhong/rn-monorepo-poc/packages/app/node_modules/react-native-gesture-handler/ios/RNGestureHandlerState.h
